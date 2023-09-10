8dfe9371a26319076319fb9a0699ef844dfedd6e
DiegoAndradeD <diegoadeiro313@gmail.com>
2023-09-10 11:01:44 -0300

    Search Form form implemented (Only visual)
    Search Form form implemented (Only visual)

--- OwlsPostReact/src/components/Help.tsx
+++ OwlsPostReact/src/components/Help.tsx
@@ -1,3 +1,5 @@
 // /* <p>Owl Icon: <a href="https://www.flaticon.com/br/icones-gratis/coruja" title="coruja ícones">Coruja ícones criados por Freepik - Flaticon</a></p>
 
 // Imagem de <a href="https://pixabay.com/pt/users/ba2design-17347071/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5539565">Ba2 Design</a> por <a href="https://pixabay.com/pt//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5539565">Pixabay</a>
+
+{/* <a href="https://www.flaticon.com/br/icones-gratis/lupa" title="lupa ícones">Lupa ícones criados por Freepik - Flaticon</a> */}
--- OwlsPostReact/src/components/Index.tsx
+++ OwlsPostReact/src/components/Index.tsx
@@ -3,29 +3,42 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import owlIcon from '../assets/owlIcon.png';
 import '../styles/Index.css'
+import searchIcon from '../assets/lupa.png'
 
 interface IndexState {
-    
+    search: string;
 }
 
 const Index: React.FC = () => {
     const navigate = useNavigate();
 
     const [state, setState] = useState<IndexState>({
-      
+      search: '',
 
     });
   
     return (
-        <div id="welcomeText">
-            <h2>Welcome to OwlsPost - Where Stories Come to Life!</h2>
-            <p>
-                Are you ready to embark on a journey through the captivating world
-                of storytelling? <br /> Look no further! This is your portal to a universe
-                of imagination, creativity, and literary adventure. <br />
-                At OwlsPost, we believe that everyone has a story to tell. 
-                Whether you're an aspiring writer, a seasoned author, or just a lover of good tales, this is the place for you.
-            </p>
+        <div className="mainDiv">
+            <div id="welcomeText">
+                <h2>Welcome to OwlsPost Where Stories Come to Life!</h2>
+                <p>
+                    Are you ready to embark on a journey through the captivating world
+                    of storytelling?
+                </p>
+            </div>
+                <div className="searchContainer">
+                    <form action="" method="get" className="searchBar">
+                        <input 
+                            type="text" 
+                            name="search" 
+                            id="search"
+                            placeholder="Search"
+                            value={state.search} 
+                            onChange={(e) => setState({...state, search: e.target.value})}/>
+
+                        <button type="submit"><img src={searchIcon} alt="searchIcon" id="searchIcon" /></button>
+                    </form>
+                </div>
         </div>
     )
 }
