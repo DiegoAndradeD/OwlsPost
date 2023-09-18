a589e0eb240c20a28f3248de1bede2f8a375e6b5
DiegoAndradeD <diegoadeiro313@gmail.com>
2023-09-18 20:07:19 -0300

    style fix

--- OwlsPostReact/src/components/User_Settings_Components/UserSettings.tsx
+++ OwlsPostReact/src/components/User_Settings_Components/UserSettings.tsx
@@ -16,23 +16,25 @@ const UserSettings: React.FC = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light" id='settings_navbar'>
-        <a className="navbar-brand" id='navbar_header_title' href="#">Settings: </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className={`nav-item ${selectedComponent === 'UserAccount_Settings' ? 'active' : ''}`} id='Settings_acountText'>
-              <a className="nav-link" href="#" onClick={() => handleClick('UserAccount_Settings')}>
-                <FontAwesomeIcon className='sidebar_icon' icon={faUser}/> Account
-              </a>
-            </li>
-            <li className={`nav-item ${selectedComponent === 'UserProfile_Settings' ? 'active' : ''}`} id='Settings_profileText'>
-              <a className="nav-link" href="#" onClick={() => handleClick('UserProfile_Settings')}>
-                <FontAwesomeIcon className='sidebar_icon' icon={faAddressCard} /> Profile
-              </a>
-            </li>
-          </ul>
+        <div className='container' id='settings_navbar_container'>
+          <a className="navbar-brand" id='navbar_header_title' href="#">Settings: </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className={`nav-item ${selectedComponent === 'UserAccount_Settings' ? 'active' : ''}`} id='Settings_acountText'>
+                <a className="nav-link" href="#" onClick={() => handleClick('UserAccount_Settings')}>
+                  <FontAwesomeIcon className='sidebar_icon' icon={faUser}/> Account
+                </a>
+              </li>
+              <li className={`nav-item ${selectedComponent === 'UserProfile_Settings' ? 'active' : ''}`} id='Settings_profileText'>
+                <a className="nav-link" href="#" onClick={() => handleClick('UserProfile_Settings')}>
+                  <FontAwesomeIcon className='sidebar_icon' icon={faAddressCard} /> Profile
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
       <div className="container mt-1" id='mainContainer'>
